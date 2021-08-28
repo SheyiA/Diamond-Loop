fb9a0cad82065e15c642ab361bf5c662b9c65bd7
SheyiA <74617018+SheyiA@users.noreply.github.com>
2021-08-28 14:56:34 -0600

    Add files via upload

---
+++ DiamondLoop.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class DiamondLoop {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of rows: ");
+        int n =sc.nextInt();
+        System.out.println("Enter the symbol: ");
+        char c = sc.next().charAt(0);
+        int i = 1;
+        int j; 
+        while(i<=n){
+            j = 1;
+            while(j++<=n-i){
+                System.out.print(" ");
+
+            }
+            j=1;
+            while(j++<=i*2-1){
+                System.out.print(c);
+            }
+            System.out.println();
+            i++;
+        }
+        i=n-1;
+        while(i>0){
+            j=1;
+            while(j++<=n-i){
+                System.out.print(" ");
+            }
+            j=1;
+            while(j++<=i*2-1)
+            {
+                System.out.print(c);
+            }
+            System.out.println();
+            i--;
+        }
+    }
+}
